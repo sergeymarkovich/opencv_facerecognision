701de5fd9031f0912ce60dcddb030fa1d5b80b24
SergeyM <sergey-markin2013@mail.ru>
2023-01-09 08:55:55 +0300

    add delay in video capture

--- face_record_dataset.py
+++ face_record_dataset.py
@@ -3,7 +3,6 @@ import os
 
 cam = cv2.VideoCapture(0)
 
-
 face_detector = cv2.CascadeClassifier('faces.xml')
 
 # For each person, enter one numeric face id
@@ -13,28 +12,36 @@ print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 # Initialize individual sampling face count
 count = 0
 
-while(True):
+while True:
 
     ret, img = cam.read()
+    fps = cam.get(cv2.CAP_PROP_FPS)
+    multiplier = fps * 3
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
 
-    for (x, y, w, h) in faces:
+    if ret:
+        frame_id = int(round(cam.get(0)))
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+            cv2.imshow('image', img)
 
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
-        count += 1
+        if frame_id % multiplier == 0:
+            count += 1
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", img[y:y + h, x:x + w])
+            print(f"Take a screenshot number {count}")
 
-        # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", img[y:y+h,x:x+w])
-        cv2.imshow('image', img)
-
-    key = cv2.waitKey(100) & 0xFF
-    if key == ord('q'):
-        break
-    elif count >= 10: # Take 10 face sample and stop video
-         break
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord(" "):
+            count += 1
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", img[y:y + h, x:x + w])
+            print(f"Take a screenshot number {count}")
+        elif key == ord('q'):
+            break
+        elif count >= 20: # Take 10 face sample and stop video
+             break
 
 # Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
+print("\n [INFO] 20 Screens taken. Exiting Program...")
 cam.release()
 cv2.destroyAllWindows()
